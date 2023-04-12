24014741bcc2689bb6859b82a44c9aa657858fe6
Nick Chapman <nchap626@gmail.com>
2023-04-12 19:18:50 -0400

    Initial Commit

---
+++ src/Map.js
@@ -0,0 +1,81 @@
+import mapboxgl from "mapbox-gl";
+import React, { useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import geoJson from "./tech-locations.json";
+import "./Map.css";
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibmNoYXA2MjYiLCJhIjoiY2xnY3VoYTQ0MHJ2aDNobnppeW85Ym5mNiJ9.AddFfspa30LJ5D0zgfmL-w";
+
+const Marker = ({ onClick, children, feature }) => {
+  const _onClick = () => {
+    onClick(feature);
+  };
+
+  return (
+    <button onClick={_onClick} className="marker">
+      {children}
+    </button>
+  );
+};
+
+const Map = () => {
+  const mapContainerRef = useRef(null);
+  const mapRef = useRef(null);
+  // Initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [-84.3958, 33.7770],
+      zoom: 15,
+    });
+
+    mapRef.current = map
+
+    // Add navigation control (the +/- zoom buttons)
+    map.addControl(new mapboxgl.NavigationControl(), "top-right");
+
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy : true
+        }, 
+        trackUserLocation : true
+      }),
+    "top-left");
+
+    // Render custom marker components
+    geoJson.features.forEach((feature) => {
+      // Create a React ref
+      const ref = React.createRef();
+      // Create a new DOM node and save it to the React ref
+      ref.current = document.createElement("div");
+      // Render a Marker Component on our new DOM node
+      ReactDOM.render(
+        <Marker onClick={markerClicked} feature={feature} />,
+        ref.current
+      );
+
+      // Create a Mapbox Marker at our new DOM node
+      new mapboxgl.Marker(ref.current)
+        .setLngLat(feature.geometry.coordinates)
+        .addTo(map);
+
+    });
+
+    // Clean up on unmount
+    return () => map.remove();
+  });
+
+  const markerClicked = (feature) => {
+    new mapboxgl.Popup(mapRef.current)
+      .setLngLat(feature.geometry.coordinates)
+      .setHTML(feature.properties.description)
+      .addTo(mapRef.current);
+  };
+
+  return <div className="map-container" ref={mapContainerRef} />;
+};
+
+export default Map;
