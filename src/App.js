0b5cc777f6a64b5a84b09fdac7bd9b254026b795
Sneed, Summer <summer.sneed@gatech.edu>
2023-04-16 17:08:42 -0400

    adding landing page, location page, and styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import Map from "./Map";
-
+import LandingPage from "./LandingPage";
+import Location from "./Location";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
-  return <Map />;
+  return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path = "/" element = {<LandingPage/>}/>
+                    <Route path={"/map"} element={<Map/>}/>
+                    <Route path={"/locations"} element={<Location/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+  )
 }
 
 export default App;
